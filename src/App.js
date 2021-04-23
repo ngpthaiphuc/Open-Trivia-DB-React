4f1a6519014d7d5aee33a96280e451cbe20944af
Thai <ngpthaiphuc@gmail.com>
2021-04-23 15:16:52 -0400

    [ADD] Basic skeleton with questions and buttons for answers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Button } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*class App {
+    onChangeValue(event) {
+        console.log(event.target.value);
+    }
+
+    render() {
+        return (
+            <div onChange={this.onChangeValue}>
+            <input type="radio" value="Yes" name="yn" /> Yes
+            <input type="radio" value="No" name="yn" /> No
+        </div>
+        );
+
+    }
+}*/
+
+export default function App() {
+    const [questions, setQuestions] = useState([]);
+    useEffect(() => {
+        fetch("https://opentdb.com/api.php?amount=3&type=multiple")
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                setQuestions(res.results);
+            });
+    }, []);
+
+    /*handleButtonInput = event => {
+        console.log(event.target.value);
+    };*/
+    //console.log(document.getElementById("question"));
+
+    return (
+        <div>
+            
+            <h1>Hey, Launch!</h1>
+            {questions.map((question) => (
+                <div>
+                    <h1 id="question">{question.question}</h1>
+
+                    <Button id="right" variant="contained" color="default" onClick={() => {
+                        alert('Correct');
+                    }}>{question.correct_answer}</Button>
+                    <Button id="wrong1" variant="contained" color="default" onClick={() => {
+                        alert('Wrong');
+                    }}>{question.incorrect_answers[0]}</Button>
+                    <Button id="wrong2" variant="contained" color="default" onClick={() => {
+                        alert('Wrong');
+                    }}>{question.incorrect_answers[1]}</Button>
+                    <Button id="wrong3" variant="contained" color="default" onClick={() => {
+                        alert('Wrong');
+                    }}>{question.incorrect_answers[2]}</Button>
+                </div>
+            ))}
+        </div>
+    );
 }
-
-export default App;
