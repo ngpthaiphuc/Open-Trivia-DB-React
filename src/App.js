7ad5512d4026432bcbf051e9b4e35dd87d4bb6dd
Thai <ngpthaiphuc@gmail.com>
2021-04-24 13:35:41 -0400

    [ADD] Randomize answer choices, disable buttons, keep track of # of questions and # of answers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Question from './Question';
-//import { Button } from '@material-ui/core';
+import { Input, Button } from '@material-ui/core';
 
 /*class App {
     onChangeValue(event) {
@@ -20,26 +20,60 @@ import Question from './Question';
 
 export default function App() {
     const [questions, setQuestions] = useState([]);
-    useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=3&type=multiple")
+    const [numQuestions, setNumQuestions] = useState(10);
+    const [numAnswered, setNumAnswered] = useState(0);
+
+    const handleChange = (event) => {
+        setNumQuestions(event.target.value);
+    }
+
+    const fetchQuestions = () => {
+        fetch("https://opentdb.com/api.php?amount=" + numQuestions)
             .then((res) => res.json())
             .then((res) => {
                 console.log(res);
-                setQuestions(res.results);
-            });
-    }, []);
+                //setQuestions(res.results);
+                const questions = res.results.map((question) => {
+                    let answers;
 
-    /*handleButtonInput = event => {
-        console.log(event.target.value);
-    };*/
-    //console.log(document.getElementById("question"));
+                    if (question.type === "boolean") {
+                        answers = ["True", "False"];
+                    } else {
+                        answers = [...question.incorrect_answers];
+                        const rand = Math.floor(Math.random() * 3);
+
+                        answers.splice(rand, 0, question.correct_answer);
+                    }
+                    return { ...question, answers };
+                });
+                setQuestions(questions);
+            });
+        setNumAnswered(0);
+    }
+
+    if (questions.length === 0) {
+        return (
+            <div>
+                <Input type="number" value={numQuestions} onChange={handleChange} />
+                <Button onClick={fetchQuestions}>Go</Button>
+            </div>
+        );
+    }
 
     return (
         <div>
             <h1>Hey, Launch!</h1>
             {questions.map((question) => (
-                <Question question={question}></Question>
+                <Question
+                    question={question}
+                    setNumAnswered={setNumAnswered}
+                    key={question.question}
+                ></Question>
             ))}
+
+            {numAnswered == parseInt(numQuestions) && (
+                <Button onClick={fetchQuestions}>More Questions</Button>
+            )}
         </div>
     );
 }
--- src/Question.js
+++ src/Question.js
@@ -1,12 +1,30 @@
 //import App from './App'
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Button } from '@material-ui/core';
 
-export default function Question({ question }) {
-    const [flagCorrect, setFlagCorrect] = useState(true);
+export default function Question({ question, setNumAnswered }) {
+    /*const [flagCorrect, setFlagCorrect] = useState(true);
     const [flagWrong1, setFlagWrong1] = useState(true);
     const [flagWrong2, setFlagWrong2] = useState(true);
-    const [flagWrong3, setFlagWrong3] = useState(true);
+    const [flagWrong3, setFlagWrong3] = useState(true);*/
+
+    const [isAnswered, setIsAnswered] = useState(false);
+
+    const handleClick = () => {
+        setIsAnswered(true);
+        setNumAnswered((prevVal) => {
+            return prevVal + 1;
+        });
+    }
+
+    const buttonColor = (answer) => {
+        if (!isAnswered)
+            return "";
+        if (answer === question.correct_answer)
+            return "green";
+        else
+            return "red";
+    }
 
     function decodeHTML(str) {
         /*var entities = {
@@ -28,36 +46,45 @@ export default function Question({ question }) {
         }).join('');
         */
         //Thanks Youtube
-        var parser = new DOMParser().parseFromString(str, "text/html")
+        let parser = new DOMParser().parseFromString(str, "text/html")
 
         return parser.documentElement.textContent;
     }
-    
-    //look into splice function for array
 
     return (
         <div>
             <h1 id="question">{decodeHTML(question.question)}</h1>
 
-            <Button id="right" variant="contained" style={{ background: flagCorrect ? "" : "green" }} onClick={() => {
-                //alert('Correct');
-                setFlagCorrect(!flagCorrect);
-            }}>{decodeHTML(question.correct_answer)}</Button>
-
-            <Button id="wrong1" variant="contained" style={{ background: flagWrong1 ? "" : "red" }} onClick={() => {
-                //alert('Wrong');
-                setFlagWrong1(!flagWrong1);
-            }}>{decodeHTML(question.incorrect_answers[0])}</Button>
-
-            <Button id="wrong2" variant="contained" style={{ background: flagWrong2 ? "" : "red" }} onClick={() => {
-                //alert('Wrong');
-                setFlagWrong2(!flagWrong2);
-            }}>{decodeHTML(question.incorrect_answers[1])}</Button>
-
-            <Button id="wrong3" variant="contained" style={{ background: flagWrong3 ? "" : "red" }} onClick={() => {
-                //alert('Wrong');
-                setFlagWrong3(!flagWrong3);
-            }}>{decodeHTML(question.incorrect_answers[2])}</Button>
+            {question.answers.map((answer) => (
+                <Button
+                    variant="contained"
+                    style={{ background: buttonColor(answer) }}
+                    onClick={handleClick}
+                    disabled={isAnswered}
+                    key={answer}
+                >{decodeHTML(answer)}</Button>
+            ))}
         </div>
     );
+    /*
+    <Button id="right" variant="contained" style={{ background: flagCorrect ? "" : "green" }} onClick={() => {
+        //alert('Correct');
+        setFlagCorrect(!flagCorrect);
+    }}>{decodeHTML(question.correct_answer)}</Button>
+
+    <Button id="wrong1" variant="contained" style={{ background: flagWrong1 ? "" : "red" }} onClick={() => {
+        //alert('Wrong');
+        setFlagWrong1(!flagWrong1);
+    }}>{decodeHTML(question.incorrect_answers[0])}</Button>
+
+    <Button id="wrong2" variant="contained" style={{ background: flagWrong2 ? "" : "red" }} onClick={() => {
+        //alert('Wrong');
+        setFlagWrong2(!flagWrong2);
+    }}>{decodeHTML(question.incorrect_answers[1])}</Button>
+
+    <Button id="wrong3" variant="contained" style={{ background: flagWrong3 ? "" : "red" }} onClick={() => {
+        //alert('Wrong');
+        setFlagWrong3(!flagWrong3);
+    }}>{decodeHTML(question.incorrect_answers[2])}</Button>
+    */
 }
